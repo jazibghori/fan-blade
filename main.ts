e8d87cdc2d6312bcef5af0c42e010e5bb6b902fa
jazibghori <77227927+jazibghori@users.noreply.github.com>
2021-01-26 17:59:48 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+let duty = 0
+basic.forever(function () {
+    while (duty < 1023) {
+        pins.analogWritePin(AnalogPin.P0, duty)
+        duty = duty + 100
+        basic.pause(10)
+    }
+    while (duty > 0) {
+        pins.analogWritePin(AnalogPin.P0, duty)
+        duty = duty - 1
+        basic.pause(10)
+    }
+    basic.pause(2000)
+})
