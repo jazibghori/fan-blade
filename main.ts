1d32650a069d23b900c52f0180036a7c54d3627f
jazibghori <77227927+jazibghori@users.noreply.github.com>
2021-01-30 13:26:26 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,20 @@
-let duty = 0
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     while (duty < 1023) {
         pins.analogWritePin(AnalogPin.P0, duty)
-        duty = duty + 100
+        duty = duty + 10
         basic.pause(10)
     }
-    while (duty > 0) {
-        pins.analogWritePin(AnalogPin.P0, duty)
-        duty = duty - 1
-        basic.pause(10)
-    }
-    basic.pause(2000)
+    basic.showIcon(IconNames.Heart)
+    pins.analogWritePin(AnalogPin.P2, 1023)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    pins.analogWritePin(AnalogPin.P2, 0)
+    while (duty > 0) {
+        duty = duty - 10
+        pins.analogWritePin(AnalogPin.P0, duty)
+        basic.pause(10)
+    }
+})
+let duty = 0
+duty = 0
